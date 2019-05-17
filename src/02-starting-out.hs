3a9c593bb719bb26489374ee7ee1dc07e62f8909
Cameron Dart <camerondart13@gmail.com>
2019-05-16 19:33:58 -0700

    02-starting-out function signatures

---
+++ src/02-starting-out.hs
@@ -0,0 +1,41 @@
+module Lib
+(
+  findK
+  , isPalindrome
+  , duplicate
+  , ziplike
+  , splitAtIndex
+  , dropK
+  , slice
+  , insertElem
+  , rotate
+) where
+
+findK :: a -> [a] -> a
+findK = undefined
+
+isPalindrome :: [a] -> [a] -> bool
+isPalindrome = undefined
+
+
+duplicate :: [a] -> [a]
+duplicate = undefined
+
+ziplike :: [a] -> [b] -> [(a, b)]
+ziplike = undefined
+
+splitAtIndex :: int -> [a] -> [a]
+splitAtIndex = undefined
+
+dropK :: int -> [a] -> [a]
+dropK = undefined
+
+slice :: int -> int -> [a] -> [a]
+slice = undefined
+
+insertElem :: a -> int -> [k]
+insertElem = undefined
+
+rotate :: int -> [a] -> [a]
+rotate = undefined
+
