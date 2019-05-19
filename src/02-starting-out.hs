4511cc0e16bc14927c0ccd01f387b8f55bbf3e46
Cameron Dart <camerondart13@gmail.com>
2019-05-18 17:35:37 -0700

    remove intero complaints

--- src/02-starting-out.hs
+++ src/02-starting-out.hs
@@ -1,6 +1,5 @@
 module Lib
-(
-  penultimate
+  ( penultimate
   , findK
   , isPalindrome
   , duplicate
@@ -10,44 +9,51 @@ module Lib
   , slice
   , insertElem
   , rotate
-) where
+  ) where
 
 penultimate :: [a] -> a
 penultimate l = last (init l)
 
 findK :: (Eq a, Eq b, Num b, Ord b) => b -> [a] -> a
-findK k l | k > 0 && l == [] = error "index longer than length"
-          | k < 0            = error "positive indicies only"
-          | k == 0           = head l
-          | otherwise        = findK (k - 1) (tail l)
+findK k l
+  | k > 0 && l == [] = error "index longer than length"
+  | k < 0 = error "positive indicies only"
+  | k == 0 = head l
+  | otherwise = findK (k - 1) (tail l)
 
-isPalindrome :: (Eq a) => [a] -> [a] -> Bool
+isPalindrome :: (Eq a) => [a] -> Bool
 isPalindrome [] = error "isPalindrome cannot handle an empty list"
+isPalindrome l = isPalindrome' l
 
+isPalindrome' :: (Eq a) => [a] -> Bool
 isPalindrome' [_] = True
-isPalindrome' l   = (head l == (last l)) && isPalindrome' tail (init l)
+isPalindrome' l   = head l == (last l) && isPalindrome' (tail (init l))
 
 duplicate :: [a] -> [a]
 duplicate []     = []
-duplicate (x:xs) = x:x:(duplicate xs)
+duplicate (x:xs) = x : x : (duplicate xs)
 
 ziplike :: [a] -> [b] -> [(a, b)]
 ziplike [] [] = []
-ziplike a  []  = a
-ziplike [] b  = b
-ziplike a  b   = (head a, head b): ziplike (tail a) (tail b)
+ziplike a _   = []
+ziplike _ b   = []
+ziplike a b   = (head a, head b) : ziplike (tail a) (tail b)
 
-splitAtIndex :: int -> [a] -> [a]
-splitAtIndex = undefined
+splitAtIndex :: Int -> [a] -> [a]
+splitAtIndex k l
+  | k < 0 = error "splitAtIndex cannot have a negative index"
+  | k == 0 = tail l
+  | otherwise = splitAtIndex (k - 1) (tail l)
 
-dropK :: int -> [a] -> [a]
-dropK = undefined
+dropK :: Int -> [a] -> [a]
+dropK k l | k < 0 = error "dropK cannot have a negative index"
+  | k > 0 && ((null l) /= True) = error "dropK cannot have index greater than length"
 
-slice :: int -> int -> [a] -> [a]
+slice :: Int -> Int -> [a] -> [a]
 slice = undefined
 
-insertElem :: a -> int -> [k]
-insertElem = undefined
+insertElem :: a -> Int -> [k]
+insertElem a k = undefined
 
 rotate :: Int -> [a] -> [a]
 rotate = undefined
