8f64fed55d37b6d714fcab315b84edcc292fe300
Cameron Dart <camerondart13@gmail.com>
2019-05-18 18:07:25 -0700

    03-types-and-typeclass

---
+++ src/03-types-and-typeclasses.hs
@@ -0,0 +1,21 @@
+module Lib
+(
+    Color(..)
+    , firstColor
+    , reverseColorOrder
+    , paintMix
+) where
+
+data Color = Red | Orange | Yellow | Green | Blue | Indigo | Violet
+    deriving (Eq, Ord, Show, Bounded, Enum)
+
+
+firstColor :: Color
+firstColor = minBound :: Color
+
+reverseColorOrder :: [Color]
+reverseColorOrder = reverse [minBound :: Color .. maxBound :: Color]
+
+paintMix :: Color -> Color -> Color
+paintMix c1 c2 = colorRange !! quot (length colorRange) 2
+  where colorRange = [c1 .. c2]
