98463e4028987b287866de1fc39f059f48987e4b
Cameron Dart <camerondart13@gmail.com>
2019-05-22 23:12:52 -0700

    functor, monoid, and semigroup implementations for custom data class

---
+++ src/11-functors-applicative-functors-and-monoids.hs
@@ -0,0 +1,29 @@
+import           Control.Applicative
+import           Data.Monoid
+
+data List a = Empty | Value a (List a)
+    deriving (Show)
+
+-- Functor
+instance Functor List where
+  fmap f (Value a x) = Value (f a) (fmap f x)
+  fmap f Empty       = Empty
+
+-- Semigroup
+-- <> is syntatic sugar for mappend
+instance Semigroup (List a) where
+    Empty       <> Empty    = Empty
+    Empty       <> x        = x
+    x           <> Empty    = x
+    (Value a x) <> y        = (Value a (x <> y))
+
+-- Monoid
+instance Monoid (List a) where
+  mempty = Empty
+
+-- Applicative
+--instance Applicative List where
+  --pure = Value
+  --Empty <*> _ = Empty
+  --(Value f _) <*> y = fmap f y
+
